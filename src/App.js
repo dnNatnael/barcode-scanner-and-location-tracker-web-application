7e13ecc2ef08e143ff90ffde46feaa0c4e6cf503
Natnael Yilma <natnael.yilma27@gmail.com>
2025-08-04 21:36:24 +0300

    Update App.js to correctly import necessary files.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import Signup from "./components/Pages/Signup";
 import Login from "./components/Pages/Login";
-import FrontPage from "./components/Pages/FrontPage";
 import LocationTracker from "./components/AdminDashboard/LocationTracker";
 import Scanner from "./components/DriverDashboard/Scanner";
-import UserTypeSelection from "./components/Pages/UserTypeSelect";
 import IT from "./components/Pages/IT";
+import ItLoginSignup from "./components/Pages/ItLoginSignup";
 import ItLogin from "./components/Pages/ItLogin";
 import ItSignup from "./components/Pages/ItSignup";
 import Resignated from "./components/Pages/Resignated";
 import DriverSampleScan from "./components/Pages/DriverSampleScan";
+import Scan from "./components/Pages/Scan";
+import NetworkStatus from "./components/Pages/NetworkStatus";
+import DriversList from "./components/Pages/DriversList";
+import Samples from "./components/Pages/Samples";
+import OneDriver from "./components/AdminDashboard/OneDriver";
+import { LocationDisplayProvider } from "./contexts/LocationDisplayContext";
+import { getDoc, doc, collection, addDoc, setDoc, serverTimestamp, runTransaction, query, orderBy, onSnapshot } from "firebase/firestore";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="whole-pages">
-      <Routes>
-        <Route path="/" element={<FrontPage />} />
-        <Route path="/choose" element={<UserTypeSelection />} />
-        <Route path="/it" element={<IT />} />
-        <Route path="/it-login" element={<ItLogin />} />
-        <Route path="/it-signup" element={<ItSignup />} />
-        <Route path="/login" element={<Login onLogin={(data) => alert(JSON.stringify(data))} />} />
-        <Route path="/signup" element={<Signup onSignup={(data) => alert(JSON.stringify(data))} />} />
-        <Route path="/admin-dashboard" element={<LocationTracker />} />
-        <Route path="/driver-dashboard" element={<Scanner />} />
-        <Route path="/resignated" element={<Resignated />} />
-        <Route path="/driver-sample-scan" element={<DriverSampleScan />} />
-      </Routes>
-    </div>
+    <LocationDisplayProvider>
+      <div className="whole-pages">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/it" element={<IT />} />
+          <Route path="/it-login" element={<ItLogin />} />
+          <Route path="/it-signup" element={<ItSignup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/network-status" element={<NetworkStatus />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin-dashboard" element={<LocationTracker />} />
+          <Route path="/driver-dashboard" element={<Scanner />} />
+          <Route path="/resignated" element={<Resignated />} />
+          <Route path="/driver-sample-scan" element={<DriverSampleScan />} />
+          <Route path="/drivers-list" element={<DriversList />} />
+          <Route path="/samples" element={<Samples />} />
+          <Route path="/one-driver" element={<OneDriver />} />
+        </Routes>
+      </div>
+    </LocationDisplayProvider>
   );
 }
 
